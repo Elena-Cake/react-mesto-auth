c730c096be0a7a9b3369c15b9ed8a9feb43cfbf9
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-02-02 17:16:46 +0300

    isPageLogin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,14 @@ import { useState } from 'react';
 
 function Header({ emailUser, signOut, showUser, openMenu, hideMenu }) {
 
+    const [isPageLogin, setIsPageLogin] = useState(false)
 
     return (
         <header className="header">
             <div className='header__menu'>
                 <img className="header__logo" src={logoPath} alt="логотип 'Место'" />
-                {!showUser ?
+                {!isPageLogin &
+                    !showUser ?
                     <button className='navbar__burger' onClick={openMenu}>
                         <div className='navbar__line'></div>
                         <div className='navbar__line'></div>
@@ -24,12 +26,12 @@ function Header({ emailUser, signOut, showUser, openMenu, hideMenu }) {
             <div className='header__navbar navbar'>
                 <Routes>
                     <Route path="/sign-up" element={
-                        <Link to={"/sign-in"} className="navbar__text navbar__text_type_link navbar__login-page">Войти</Link>} />
+                        <Link to={"/sign-in"} className="navbar__text navbar__text_type_link navbar__login-page" setPageLogin={setIsPageLogin(true)}>Войти</Link>} />
                     <Route path="/sign-in" element={
-                        <Link to={"/sign-up"} className="navbar__text navbar__text_type_link navbar__login-page">Регистрация</Link>} />
+                        <Link to={"/sign-up"} className="navbar__text navbar__text_type_link navbar__login-page" setPageLogin={setIsPageLogin(true)}>Регистрация</Link>} />
                     <Route path="/" element={
                         <div className={`navbar__main ${showUser ? 'navbar__main_open' : ''}`}>
-                            <p className='navbar__text'>{emailUser}</p>
+                            <p className='navbar__text' setPageLogin={setIsPageLogin(false)}>{emailUser}</p>
                             <button className='navbar__text navbar__text_type_link' onClick={signOut}>Выйти</button>
                         </div>} />
                 </Routes>
